f8b0f15b2c15e0490bc9b29ef76bf4feb8a363a3
epshita-shima <epshitashima@gmail.com>
2022-06-29 20:37:55 +0600

    load data from server

---
+++ src/components/Table/Table.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import './Table.css';
+
+const Table = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/info')
+            .then(res => res.json())
+            .then(data => setData(data));
+    }, [])
+    return (
+        <div className='container'>
+            <h2 className='mt-5 text-center'>Books store</h2>
+
+            <div className='table-fixed'>
+                <table class="table table-bordered mt-4">
+                    <thead>
+                        <tr>
+                            <th scope="col">Sl.</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Original_language</th>
+                            <th scope="col">Publish_date</th>
+                            <th scope="col">Genres</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((d, index) => <tr>
+                                <td>{index + 1}</td>
+                                <td>{d.author}</td>
+                                <td>{d.title}</td>
+                                <td>{d.original_language}</td>
+                                <td>{d.publish_date}</td>
+                                <td>{d.genres}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Table;
